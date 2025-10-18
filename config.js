e6485d9523ae60f584c5770deeba37a852183105
rasyendriar <73742275+rasyendriar@users.noreply.github.com>
2025-10-18 11:41:36 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAeHlByuYsOB4TFOt55gkhL-beg7sjgyu8",
+    authDomain: "machine-dashboard-app.firebaseapp.com",
+    projectId: "machine-dashboard-app",
+    storageBucket: "machine-dashboard-app.firebasestorage.app",
+    messagingSenderId: "936861896173",
+    appId: "1:936861896173:web:c5e64f35604f84498c6be4"
+};
+
+export const googleApiConfig = {
+    apiKey: "AIzaSyBcQyXp0ZgZ4zAfe3wQ8cLR3UUZxZwfJW8",
+    clientId: "936861896173-mjigkv1uq44j3ase7li4ganluuqjf2dk.apps.googleusercontent.com",
+    folderUrl: "https://drive.google.com/drive/folders/1SUL4zcjEA18x9l7jzuarP2vsNS3FsOL_"
+};

d36d79f1c10b7ff8d782a6fedd336aab69087780
rasyendriar <73742275+rasyendriar@users.noreply.github.com>
2025-10-20 13:30:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,10 @@ export const firebaseConfig = {
     projectId: "machine-dashboard-app",
     storageBucket: "machine-dashboard-app.firebasestorage.app",
     messagingSenderId: "936861896173",
-    appId: "1:936861896173:web:c5e64f35604f84498c6be4"
+    appId: "1:936861896173:web:c5e64f84498c6be4"
 };
 
 export const googleApiConfig = {
     apiKey: "AIzaSyBcQyXp0ZgZ4zAfe3wQ8cLR3UUZxZwfJW8",
     clientId: "936861896173-mjigkv1uq44j3ase7li4ganluuqjf2dk.apps.googleusercontent.com",
-    folderUrl: "https://drive.google.com/drive/folders/1SUL4zcjEA18x9l7jzuarP2vsNS3FsOL_"
 };
